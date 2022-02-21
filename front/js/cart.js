8d74e52cbe51fb6f22c3ce3098177d0203f898ef
Ilir <ajdini.ilir.91@gmail.com>
2022-02-21 15:20:38 +0100

    Update cart.js
    Début du panier on a juste chercher les éléments

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,2 +1,21 @@
-//Validation du pannier/ afficher le prix total/ rentrer les coordonnées/ valider la commande
+//Cherche le produit dans le local storage
+let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
+//les produits du localStorage tableau 
+console.log(produitLocalStorage);
+
+//Selectionné ou injecter
+let cartItem = document.getElementById("cart__items")
+let totalQuantity = document.getElementById("totalQuantity")
+let totalPrice = document.getElementById("totalPrice")
+
+console.log(cartItem);
+console.log(totalQuantity);
+console.log(totalPrice);
+
+//Si le panier est vide : afficher le panier est vide
+if (produitLocalStorage === null) {
+    alert("Le panier est vide")
+} else {
+    console.log('ya des trucs');
+}//Validation du pannier/ afficher le prix total/ rentrer les coordonnées/ valider la commande
 
