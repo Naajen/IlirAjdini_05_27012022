68f56fdeb65a850e53ccef3dcab7f2dca8c28aba
Ilir <ajdini.ilir.91@gmail.com>
2022-02-23 10:40:57 +0100

    Update cart.js
    Reduction du code JS pour le total quantité et prix
    
    Début de la condition de la page cart si le panier est  vide celui ci sera redirigé vers la page index si le panier est vide. sinon il peut continuer sur la page panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,67 +1,66 @@
 //Cherche le produit dans le local storage
 let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
-//les produits du localStorage tableau 
 
 //Selectionné ou injecter
 let recapPanier = document.querySelector("#cart__items")
 let totalQuantity = document.getElementById("totalQuantity")
 let totalPrice = document.getElementById("totalPrice")
-
-for (i = 0; i < produitLocalStorage.length; i++) {
-    recapPanier.innerHTML +=
-    `<article class="cart__item" data-id=${produitLocalStorage[i].id} data-color=${produitLocalStorage[i].colors}>
-        <div class="cart__item__img">
-            <img src=${produitLocalStorage[i].image} alt="${produitLocalStorage[i].altTxt}é">
-        </div>
-            <div class="cart__item__content">
-                <div class="cart__item__content__description">
-                    <h2>Produit : ${produitLocalStorage[i].name}</h2>
-                    <p>Couleur : ${produitLocalStorage[i].colors}</p>
-                    <p>${produitLocalStorage[i].prix} €</p>
-                </div>
-                <div class="cart__item__content__settings">
-                <div class="cart__item__content__settings__quantity">
-                    <p>Qté :</p>
-                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${produitLocalStorage[i].quantity}>
-                </div>
-                <div class="cart__item__content__settings__delete">
-                    <p class="deleteItem">Supprimer</p>
+let suppBtn = document.getElementsByClassName("deleteItem")
+// Affiche les produits du localStorage dans le DOM
+const lePanier = () => {
+    for (i = 0; i < produitLocalStorage.length; i++) {
+        recapPanier.innerHTML +=
+        `<article class="cart__item" data-id=${produitLocalStorage[i].id} data-color=${produitLocalStorage[i].colors}>
+            <div class="cart__item__img">
+                <img src=${produitLocalStorage[i].image} alt="${produitLocalStorage[i].altTxt}é">
+            </div>
+                <div class="cart__item__content">
+                    <div class="cart__item__content__description">
+                        <h2>Produit : ${produitLocalStorage[i].name}</h2>
+                        <p>Couleur : ${produitLocalStorage[i].colors}</p>
+                        <p>${produitLocalStorage[i].prix} €</p>
+                    </div>
+                    <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                        <p>Qté : </p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${produitLocalStorage[i].quantity}>
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                        <p class="deleteItem">Supprimer</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </article>`
+        </article>`
+    
+    }
+    console.log(suppBtn);
 
+    /*****************************TOTAL QUANTITE & PRIX DU PANIER**********************************/
+    //une déclaration de reducer permet d'additionné toutes les valeurs du tableau
+    const reducer = (accumulator, currentValue) => accumulator + currentValue
+    //déclarer un tableau vide pour y mettre une valeur ici c'est la quantité total
+    let totalArticle = [];
+    let totalPrix = [];
+    //parcourir le tableau
+    for (k = 0; k < produitLocalStorage.length; k++) {
+        //crée une variable de l'ensemble de la quantité du tableau
+        let quantityTotal = produitLocalStorage[k].quantity;
+        //grâce au push j'envoi ce tableau dans le tableau au dessus
+        totalArticle.push(quantityTotal);
+        let quantityPrix = produitLocalStorage[k].prix;
+        totalPrix.push(quantityPrix);
+    }
+    //une fois le tableau rempli on les additionnes grâce au reducer plus haut avec une valeur initial de 0 pour évité les erreurs dans la console
+    const allArticles = totalArticle.reduce(reducer,0)
+    const allPrice = totalPrix.reduce(reducer,0)
+    //on injecte le montant total dans la bonne balise HTML
+    totalPrice.innerHTML = `<span id="totalPrice">${allPrice}</span>`
+    totalQuantity.innerHTML = `<span id="totalQuantity">${allArticles}</span>`
 }
-//déclarer un tableau pour y mettre la quantité total
-let totalArticle = [];
-for (k = 0; k < produitLocalStorage.length; k++) {
-    //crée une variable de l'ensemble de la quantité
-    let quantityTotal = produitLocalStorage[k].quantity;
-    //envoi la quantité dans un tableau
-    totalArticle.push(quantityTotal)
-}
-const reducer = (accumulator, currentValue) => accumulator + currentValue
-const allArticles = totalArticle.reduce(reducer,0)
-totalQuantity.innerHTML = `<span id="totalQuantity">${allArticles}</span>`
-
-console.log(produitLocalStorage);
-
-let totalPrix = [];
-
-for (j = 0; j < produitLocalStorage.length; j++) {
-    let quantityPrix = produitLocalStorage[j].prix;
-    totalPrix.push(quantityPrix)
-    console.log(totalPrix);
-}
-const allPrice = totalPrix.reduce(reducer,0)
-totalPrice.innerHTML = `<span id="totalPrice">${allPrice}</span>`
-
-
-/*//Si le panier est vide : afficher le panier est vide
+//Si panier vide retour à la page d'acceuil
 if (produitLocalStorage === null) {
-    alert("Le panier est vide")
+    alert("Veuillez séléctionné un produit pour continuer vos achats, vous allez être redirigé vers la page d'acceuil")
+    window.location.href = "index.html";
 } else {
-
-}*/
-
-//totalQuantity.innerHTML += `<span id="totalQuantity">${produitLocalStorage[k].quantity}</span>`
+    lePanier();
+}
