b2172d2365063284fb9f2acaf0baaee01bfd395a
Ilir <ajdini.ilir.91@gmail.com>
2022-02-24 14:42:44 +0100

    Update cart.js
    MàJ du prix en fonction de la quantité et un refresh du prix afficher

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,8 +5,8 @@ let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
 let recapPanier = document.querySelector("#cart__items")
 let totalQuantity = document.getElementById("totalQuantity")
 let totalPrice = document.getElementById("totalPrice")
-let suppBtn = document.getElementsByClassName("deleteItem")
-// Affiche les produits du localStorage dans le DOM
+
+// Affiche les produits du localStorage sur la page panier
 const lePanier = () => {
     for (i = 0; i < produitLocalStorage.length; i++) {
         recapPanier.innerHTML +=
@@ -18,11 +18,11 @@ const lePanier = () => {
                     <div class="cart__item__content__description">
                         <h2>Produit : ${produitLocalStorage[i].name}</h2>
                         <p>Couleur : ${produitLocalStorage[i].colors}</p>
-                        <p>${produitLocalStorage[i].prix} €</p>
+                        <p>Prix : ${produitLocalStorage[i].prix*produitLocalStorage[i].quantity} €</p>
                     </div>
                     <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
-                        <p>Qté : </p>
+                        <p>Qté :</p>
                         <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${produitLocalStorage[i].quantity}>
                     </div>
                     <div class="cart__item__content__settings__delete">
@@ -31,10 +31,41 @@ const lePanier = () => {
                 </div>
             </div>
         </article>`
-    
     }
-    console.log(suppBtn);
 
+    let valeurAjouter = document.querySelectorAll(".itemQuantity");
+    for (let j = 0; j < valeurAjouter.length; j++) {
+        valeurAjouter[j].addEventListener('click', (events) => {
+            let prix = produitLocalStorage[j].prix;
+            let quantity = produitLocalStorage[j].quantity;
+            let quantity2 = produitLocalStorage[j].quantity++ + 1;
+            let prix2 = produitLocalStorage[j].prix*quantity2;
+            quantity2;
+            prix2;
+            localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+            //document.location.reload()
+        });
+    }
+
+    //Parcours le tableau du btn supprimer **************SUPPRIME UN ELEMENT GRACE EN FONCTION DE LA COULEUR*****************
+    const supprimer = () => {    
+        let suppBtn = document.querySelectorAll(".deleteItem")
+        for (let l = 0; l < suppBtn.length; l++){
+            //Au click du btn on supprime un éléments sur la page & dans le local storage
+            suppBtn[l].addEventListener('click', (events) => {
+                events.preventDefault();
+                let colorSelect = produitLocalStorage[l].colors;
+                //supprimer la selection avec filter() mais en version contraire avec le !==
+                produitLocalStorage = produitLocalStorage.filter(el => el.colors !== colorSelect);
+                //Met à jour le localstorage
+                localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+                
+                alert('Produit supprimé')
+                window.location.href = "cart.html";
+            });
+        }
+    }    
+    supprimer ();
     /*****************************TOTAL QUANTITE & PRIX DU PANIER**********************************/
     //une déclaration de reducer permet d'additionné toutes les valeurs du tableau
     const reducer = (accumulator, currentValue) => accumulator + currentValue
@@ -47,8 +78,9 @@ const lePanier = () => {
         let quantityTotal = produitLocalStorage[k].quantity;
         //grâce au push j'envoi ce tableau dans le tableau au dessus
         totalArticle.push(quantityTotal);
+
         let quantityPrix = produitLocalStorage[k].prix;
-        totalPrix.push(quantityPrix);
+        totalPrix.push(quantityPrix * quantityTotal);
     }
     //une fois le tableau rempli on les additionnes grâce au reducer plus haut avec une valeur initial de 0 pour évité les erreurs dans la console
     const allArticles = totalArticle.reduce(reducer,0)
@@ -57,10 +89,13 @@ const lePanier = () => {
     totalPrice.innerHTML = `<span id="totalPrice">${allPrice}</span>`
     totalQuantity.innerHTML = `<span id="totalQuantity">${allArticles}</span>`
 }
+
+
 //Si panier vide retour à la page d'acceuil
-if (produitLocalStorage === null) {
+if (produitLocalStorage === null || produitLocalStorage == 0 || produitLocalStorage == []) {
     alert("Veuillez séléctionné un produit pour continuer vos achats, vous allez être redirigé vers la page d'acceuil")
     window.location.href = "index.html";
 } else {
+    //sinon on peut acceder au panier 
     lePanier();
-}
+} // prochaine étape le formulaire bien rempli alors le button commander
